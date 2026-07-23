b65220ba73d289ea5cf395ce42a356dcb817b98c
jackjssp <jackjssp0@gmail.com>
2026-07-22 22:27:51 -0400

    full stationary circle

--- src/auvc_pid/auvc_pid/tracking_circle.py
+++ src/auvc_pid/auvc_pid/tracking_circle.py
@@ -4,28 +4,28 @@ from sensor_msgs.msg import Imu
 from std_msgs.msg import Float64
 import time
 from auvc_pid.pid_loop import *
-from geometry_msgs.msg import Vector3  
+import numpy as np
 
 class CircularNode(Node):
     def __init__(self):
         super().__init__('angular_node')
-        
+
+        self.desired_period = 4.0 #seconds
+        self.target_radius = 2.0 #meters
+
         #all angular velocity units are in radians per second
         self.latest_angular_velocity = 0.0
-        self.target_angular_velocity = 0.0
+        self.target_angular_velocity = 2 * np.pi / self.desired_period
 
         self.latest_centripital_acceleration = 0.0
-        self.target_centripital_acceleration = 0.0
+        self.target_centripital_acceleration = (self.target_angular_velocity ** 2) * self.target_radius
 
-        self.target_radius = 2.0 #units in meters
-
-        "publishes yaw, surge, and sway values to maintain the circle"
+        #publishes yaw, surge, and sway values to maintain the circle
         self.circular_pub = self.create_publisher(Vector3, '/circle_commands', 10)
         self.imu_sub = self.create_subscription(Imu, "/imu", self.imu_callback, 10)        
         self.radius_sub = self.create_subscription(Float64, "/target_radius", self.radius_callback, 10)    
 
         self.omega_errors = []
-        self.centripetal_errors = []
 
         # run loop at 20 hz
         self.timer_period = 0.05
@@ -41,12 +41,13 @@ class CircularNode(Node):
         self.target_radius = msg.data
 
     def circle(self):
+        #update errors
         omega_error = calculate_omega_error(self.latest_heading, self.target_heading)
-        centripital_error = calculate_centripital_error(self.latest_ac, self.target_ac)
         self.omega_errors.append(omega_error)
-        
+
+        #calculate values
         yaw = calculate_yaw(self.omega_errors, self.timer_period)
-        surge = calculate_surge(self.centripetal_errors, self.timer_period)
+        sway = 100
 
         #print statements for debugging
         print(f"yaw: {yaw}")
@@ -55,14 +56,13 @@ class CircularNode(Node):
 
         # Publish the active step's joystick values
         msg = Vector3()
-        msg.x = surge
         msg.y = sway
         msg.z = yaw
         self.pub.publish(msg)
 
 def main(args=None):
     rclpy.init(args=args)
-    node = AngularNode()
+    node = CircularNode()
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
@@ -75,9 +75,6 @@ def main(args=None):
 def calculate_omega_error(current_omega, target_omega):
     return target_omega - current_omega
 
-def calculate_centripetal_error(current_ac, target_ac):
-    return target_ac - current_ac
-
 def calculate_yaw(errors, dt):
     Kp = 1.1
     Ki = 0.0
@@ -87,12 +84,11 @@ def calculate_yaw(errors, dt):
     pid_final = run_pid(errors, dt, Kp, Ki, Kd)
     yaw = pid_final * multiplier
 
-    if (abs(yaw) > 300.0):
-        if (yaw > 0):
-            return -300.0
-        else:
-            return 300.0
+    return output_cap(300, yaw)
 
-    return yaw
-
-def calculate_surge(errors, dt):
+def output_cap(output, maximum):
+    if (abs(output) > maximum):
+        if (output > 0):
+            return maximum
+        return -maximum
+    return output
