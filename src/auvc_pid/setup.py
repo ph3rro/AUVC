b70f2e39045fb8b4be5c408660626175d5b3e759
sajanibrown <sajanibrown@gmail.com>
2026-07-24 14:49:55 -0400

    Include launch files in setup.py data_files
    Added launch files to data_files in setup.py

--- src/auvc_pid/setup.py
+++ src/auvc_pid/setup.py
@@ -12,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')), #idk if this is right but should be
     ],
     install_requires=['setuptools'],
     zip_safe=True,
